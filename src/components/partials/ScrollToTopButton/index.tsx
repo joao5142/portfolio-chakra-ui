c61b510793c5a3ab30d21fd6583e9a9c53df7d60
joao5142 <joaopauloneto3687@gmail.com>
2024-07-22 11:26:25 -0300

    build: fix build ts errors

--- src/components/partials/ScrollToTopButton/index.tsx
+++ src/components/partials/ScrollToTopButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowUpIcon } from "@chakra-ui/icons";
 import { Box, Button } from "@chakra-ui/react";
 
